ca891abb81fa43d37e82c0cc12f61deff8ef1737
Bhargavi <bhargavi.manusani@reportgarden.com>
2017-05-08 09:01:03 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientApp::Application.config.secret_token = '141920a12660ceb356a245572fec5ba68283f68c18b84967d35fea60b1f339533ca23255b34f5c5ae093acd4f5127a658df6a2600595b3e24a81f308e11bbd1f'
